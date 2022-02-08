745f6749badccd5c2c73dbd0b3ed0f25e46b8674
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-08 22:40:27 +0100

    added get methods

--- src/app/service/latest-news-api.service.ts
+++ src/app/service/latest-news-api.service.ts
@@ -6,10 +6,29 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LatestNewsApiService {
+  api_key = 'pub_41402da7159f969899505b97cd9fbe8df5b8';
+
   //newsApiUrl
   newsApiUrl =
     'https://newsdata.io/api/1/news?apikey=pub_41402da7159f969899505b97cd9fbe8df5b8&q=dogecoin';
+
   constructor(private http: HttpClient) {}
+
+  initSources() {
+    return this.http.get(this.newsApiUrl);
+  }
+  
+  initArticles() {
+    return this.http.get(
+      'https://newsdata.io/api/1/news?apikey=pub_41402da7159f969899505b97cd9fbe8df5b8&country=ng,category=sports'
+    );
+  }
+  getArticleByID() {
+    return this.http.get(
+      'https://newsdata.io/api/1/news?apikey=pub_41402da7159f969899505b97cd9fbe8df5b8&category=sports'
+    );
+  }
+
   topHeader(): Observable<any> {
     return this.http.get(this.newsApiUrl);
   }
