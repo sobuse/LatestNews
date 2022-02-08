0f2fee3b308874251f3021f7b74993fd89a136d1
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-08 22:39:56 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LatestNewsApiService } from './service/latest-news-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'LatestNews';
+  mArticles: any[] = [];
+  mSources: any[] = [];
+  constructor(private newsapi:LatestNewsApiService) {
+    console.log('app component')
+  }
 }
